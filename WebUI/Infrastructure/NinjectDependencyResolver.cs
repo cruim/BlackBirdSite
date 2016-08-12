e6561a43f7d92681b3d9f18adb1a2d4dde769ae0
Daniil <hedgehog147@gmail.com>
2016-08-12 18:04:08 +0300

    app on asp.net mvc

---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type servicesType)
+        {
+            return kernel.GetAll(servicesType);
+        } 
+    }
+}
